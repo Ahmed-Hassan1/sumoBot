c135e89610bf45e3a63ef3ba080a12ae3046deda
Ahmed-Hassan1 <66271731+Ahmed-Hassan1@users.noreply.github.com>
2024-07-26 09:55:43 +0300

    Delete SpeedApp directory

--- SpeedApp/speed/jobs/updater.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import *
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(job_1,trigger='cron',minute='*/5')
-    scheduler.start()
