970dcd0b30348b86eb187acf16358a1b687af6cd
Ahmed-Hassan1 <66271731+Ahmed-Hassan1@users.noreply.github.com>
2024-07-26 09:55:55 +0300

    Delete SumoBot111 directory

--- SumoBot111/jobs/updater.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import *
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(job_1,'interval',seconds=2)
-    scheduler.add_job(FutureIndicatorFourSupport,trigger='cron',minute='*/5')
-    scheduler.start()
